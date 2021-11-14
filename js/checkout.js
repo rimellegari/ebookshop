238a009054a9e24ea6c4b9f02f3e0946f67c7221
Ricardo Mellegari <rimellegari@gmail.com>
2021-11-13 23:34:53 -0300

    finalização da página de checkout e responsividade

--- js/checkout.js
+++ js/checkout.js
@@ -4,25 +4,48 @@
         livroCart = JSON.parse(livroCart)
 
         let livroContainer = document.querySelector('#livros-container')
+        let precoCart = localStorage.getItem('totalCompras')
         if(livroCart && livroContainer) {
             livroContainer.innerHTML = '';
             Object.values(livroCart).map(livro =>{
                 livroContainer.innerHTML += `
                 <div class='produtos'>
                     <div class ='livros'>
-                    <i class="fa-light fa-circle-xmark" id="cancela"></i>
+            
                         <div class = 'titulo'>
-                        <img src='./assets/${livro.tag}.jpg'>
-                        <p>${livro.titulo}</p>
+                        <div><a class="cancela">X</a>
+                        </div>
+                            <div id="obra"><img src='./assets/${livro.tag}.jpg'>
+                            <p>${livro.titulo}</p>
+                            </div>
+
+                        </div>
+                        
+                        <div id="preco"><b>R$ ${livro.preco},00</b>
+                        </div>
+                        <div id="quantidade"><b>${livro.noCart}</b>
+                        </div>
+                        <div id="total"><h5>R$${livro.noCart * livro.preco},00</h5>
                         </div>
                     </div>
-
-
-
                 </div>
 
                 `
-            })
+            });
+
+            livroContainer.innerHTML += `
+            <div class="totalCompras">
+                
+                <p class="totalComprastitulo">
+                    Total Compras:${' '}
+                </p>
+                <p class "totalValor">
+                      R$ ${precoCart},00
+                </p>
+
+
+            
+            `
 
         }
     }
